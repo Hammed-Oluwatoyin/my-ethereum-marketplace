38b33f4380c8c9488bedf1c168f2530d4abc50a0
Dauda Hammed Oluwatoyin <dauda.hammed132@gmail.com>
2022-10-08 23:49:38 +0100

    building pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,10 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  extend: {
+    maxWidth: {
+      "8xl": "1920px",
+    },
   },
   plugins: [],
 };

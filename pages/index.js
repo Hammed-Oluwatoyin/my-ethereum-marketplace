2fbc988112520d11e533346d495835fb7ad5234d
Dauda Hammed Oluwatoyin <dauda.hammed132@gmail.com>
2022-10-10 23:51:35 +0100

    course page

--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,17 @@ import { Navbar, Footer, Hero, Breadcrumbs } from "@components/common";
 import { EthRates, WalletBar } from "@components/web3";
 import { CourseList } from "@components/course";
 import { OrderCard } from "@components/order";
+import { BaseLayout } from "@components/layout";
 
 export default function Home() {
   return (
-    <div>
-      <div className="relative bg-white overflow-hidden">
-        <div className="relative max-w-7xl mx-auto px-4">
-          {/*------ NAVBAR STARTS ------*/}
-          <Navbar />
-          {/*------ NAVBAR ENDS ------*/}
-          <div className="fit">
-            <Hero />
-            <Breadcrumbs />
-            <WalletBar />
-            <EthRates />
-
-            {/*------ ORDER INFO ENDS ------*/}
-            <OrderCard />
-            {/*------ COURSE CARD STARTS ------*/}
-            <CourseList />
-            {/*------ COURSE CARD ENDS ------*/}
-          </div>
-        </div>
-        {/*------ FOOTER STARTS ------*/}
-        <Footer />
-        {/*------ FOOTER ENDS ------*/}
-      </div>
-    </div>
+    <BaseLayout>
+      <Hero />
+      <Breadcrumbs />
+      <WalletBar />
+      <EthRates />
+      <OrderCard />
+      <CourseList />
+    </BaseLayout>
   );
 }

1e50fc501a32f481865871d6a415a03261231bad
Dauda Hammed Oluwatoyin <dauda.hammed132@gmail.com>
2022-10-12 23:51:43 +0100

    course content

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,26 @@
-import { Navbar, Footer, Hero, Breadcrumbs } from "@components/common";
-import { EthRates, WalletBar } from "@components/web3";
-import { CourseList } from "@components/course";
-import { OrderCard } from "@components/order";
-import { BaseLayout } from "@components/layout";
+import { Hero } from "@components/common";
 
-export default function Home() {
+import { CourseList } from "@components/course";
+
+import { BaseLayout } from "@components/layout";
+import { getAllCourses } from "@content/courses/fetcher";
+export default function Home({ courses }) {
   return (
-    <BaseLayout>
+    <>
       <Hero />
-      <Breadcrumbs />
-      <WalletBar />
-      <EthRates />
-      <OrderCard />
-      <CourseList />
-    </BaseLayout>
+
+      <CourseList courses={courses} />
+    </>
   );
 }
+
+export function getStaticProps() {
+  const { data } = getAllCourses();
+  return {
+    props: {
+      courses: data,
+    },
+  };
+}
+
+Home.Layout = BaseLayout;

4283f594fa7c09d491fd40dd3259bc1cf28177d4
Dauda Hammed Oluwatoyin <dauda.hammed132@gmail.com>
2022-10-15 23:10:40 +0100

    course page

--- components/course/hero/index.js
+++ components/course/hero/index.js
@@ -1,6 +1,7 @@
+import Image from "next/image";
 import React from "react";
 
-const hero = () => {
+const hero = ({ title, description, image }) => {
   return (
     <section>
       <div className="relative bg-white overflow-hidden">
@@ -25,9 +26,7 @@ const hero = () => {
                   </span>
                 </h1>
                 <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                  qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                  occaecat fugiat aliqua.
+                  {description}
                 </p>
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                   <div className="rounded-md shadow">
@@ -52,10 +51,11 @@ const hero = () => {
           </div>
         </div>
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-          <img
+          <Image
             className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-            src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-            alt=""
+            src={image}
+            alt={title}
+            layout="fill"
           />
         </div>
       </div>
